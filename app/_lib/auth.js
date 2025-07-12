5034e2aba5f7e8d9d94155900857af8813ff844e
Yossef weal <yweal423@gmaol.com>
2025-07-12 18:27:11 +0300

    12/7 in work

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -9,15 +10,33 @@ const authConfig = {
     }),
   ],
   callbacks: {
-    authorized({ auth, request })
-    {
-      return !!auth?.user
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+    async signIn({ user }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+
+      return session;
     },
   },
   pages: {
-    signIn: "/login"
+    signIn: "/login",
   },
-
 };
 
 export const {
--- app/account/page.js
+++ app/account/page.js
@@ -4,15 +4,15 @@ export const metadata = {
   title: "Account",
 };
 
-async function page()
-{
+async function page() {
   const session = await auth();
   const firstName = session.user.name.split(" ").at(0);
 
-
-  return <h2 className="font-semibold text-2xl text-accent-400 md-7">
-    Welcome, {firstName}
-  </h2>
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 md-7">
+      Welcome, {firstName}
+    </h2>
+  );
 }
 
 export default page;
